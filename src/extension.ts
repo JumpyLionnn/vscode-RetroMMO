fa89040d7c95bb2727ec4587c559c5e109bfe901
JumpyLionnn <fwipsity@gmail.com>
2022-04-11 13:06:55 +0300

    added an extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "retrommo" is now active!');
+	let disposable = vscode.commands.registerCommand('retrommo.open', () => {
+		const panel = vscode.window.createWebviewPanel("retrommo", "RetroMMO", vscode.ViewColumn.Beside, {
+			enableScripts: true,
+			enableForms: true
+		});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('retrommo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RetroMMO!');
+		// And set its HTML content
+		panel.webview.html = getWebviewContent();
+		panel.iconPath = vscode.Uri.file(context.extensionPath + '/assets/retrommo.png');
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>RetroMMO</title>
+		<style>
+			*{border: 0;}
+			body, html{margin:0;padding:0;height: 100vh;}
+			#game{
+				width: 100%;
+				height: 100vh;
+			}
+		</style>
+	</head>
+	<body>
+	<iframe id="game" src="https://play.retro-mmo.com"></iframe>
+	</body>
+	</html>`;
 }
 
 // this method is called when your extension is deactivated
